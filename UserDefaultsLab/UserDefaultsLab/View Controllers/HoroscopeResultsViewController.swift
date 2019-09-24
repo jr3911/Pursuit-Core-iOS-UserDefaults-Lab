0b5afdd016cfcf99cb4809fa126f54674a520fc1
Jason Ruan <jasonruan@pursuit.org>
2019-09-24 16:03:31 -0400

    Adds new view controller files and assigns them to corresponding view controllers on storyboard

---
+++ UserDefaultsLab/UserDefaultsLab/View Controllers/HoroscopeResultsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HoroscopeResultsViewController.swift
+//  UserDefaultsLab
+//
+//  Created by Jason Ruan on 9/24/19.
+//  Copyright © 2019 Jason Ruan. All rights reserved.
+//
+
+import UIKit
+
+class HoroscopeResultsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
